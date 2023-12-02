9a23efafcb5dcddbab5424b220f56d89385e9f28
Karxem <tippjoshua@gmx.de>
2023-12-02 13:46:38 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+public class AdventOfCode
+{
+    public static void Main(string[] args)
+    {
+        List<int> validNumbers = new List<int>();
+        string path = @"E:\Documents\GitHub\AdventOfCode\assets\input.txt";
+        string[] strings = File.ReadAllText(path).Split(
+            new string[] { Environment.NewLine },
+            StringSplitOptions.None
+        );
+        
+        foreach ( string s in strings )
+        {
+            string[] numbers = Regex.Split(s, @"\D+");
+            string number = string.Join("", numbers);
+            int i = int.Parse(number[0].ToString() + number[number.Length - 1].ToString());
+
+            validNumbers.Add(i);
+        }
+
+        Console.WriteLine("Summ: " + validNumbers.Sum());
+    }
+}
